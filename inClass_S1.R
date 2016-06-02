f4c649984eab5cd34a9a29975feb5b9e65daf9fa
LiNk-NY <mramos09@gmail.com>
2016-06-02 15:45:48 -0400

    include inclass code

---
+++ inClass_S1.R
@@ -0,0 +1,42 @@
+# Code to run in class
+
+Help()
+help()
+
+# Source code
+help
+
+h el p ()
+help    (   topic   =    "mean")
+
+`+`(1, 3)
+1 + 3
+
+a <- c(1, 3, 4, 6)
+a + 1
+
+object.size(a)
+
+?mean
+help("mean")
+
+?reserved
+
+?rnorm()
+b <- rnorm(20)
+mean(b)
+
+# Arguments
+?cor
+
+c <- rnorm(20)
+cor(b, c)
+
+# Value section
+?mean
+
+getwd()
+# setwd()
+
+babies <- read.table("Data/babies.txt")
+babies <- read.table("Data/babies.txt", header = TRUE)
