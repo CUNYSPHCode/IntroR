c532f312c102ad0dee4d9d4cad139245c56ee240
LiNk-NY <mramos09@gmail.com>
2016-06-13 20:19:04 -0400

    Additional notes for InClass file

--- InClass/InClass_S3.R
+++ InClass/InClass_S3.R
@@ -8,6 +8,13 @@ factor(sampleRanks, levels = c("low", "medium", "high"))
 
 sampRank <- factor(sampleRanks, levels = c("high", "medium", "low"))
 
+levels(sampRank) <- c("a", "b", "c")
+
+sampRank
+
+## Renaming factors
+levels(x) <- c("new", "names")
+
 ## converting numeric factors to JUST numeric (tricky)
 
 fac <- factor(as.character(10:18))
@@ -22,7 +29,7 @@ as.numeric(sampRank)
 
 ## Vector subsets
 vec <- c("a" = 1, "b" = 2, "c" = 3)
-
+vec
 vec["a"]
 
 vec[1]
@@ -52,18 +59,26 @@ aList$b
 mtcars$hp
 
 ## Using Conditions to subset
+mtcars$mpg > 20
+
+sum(mtcars$mpg > 20)
+
+mtcars$highMPG <- mtcars$mpg > 20
+
+mtcars
 
 mtcars[ mtcars$mpg > 20, ]
-airquality[ airquality$Month == "May", ]
-
-# Sorting data
-
-airquality
 
 airquality$Month <- factor(airquality$Month,
                            levels = 5:9,
                            labels = c("May", "June", "July",
                                       "August", "September"))
+
+airquality[ airquality$Month == "May", ]
+
+# Sorting data
+
+airquality
 
 order(airquality$Temp)
 
@@ -79,7 +94,7 @@ arrange(airquality, -Temp)
 
 ## Checking for duplicated rows (none found)
 duplicated(airquality)
-
+head(airquality)
 
 unique(airquality$Day)
 
@@ -91,10 +106,15 @@ airquality[complete.cases(airquality) ,]
 ## Aggregate data
 
 ## formula notation
+class(a ~ b)
 
 aggregate(. ~ Month, data = airquality, mean)
 
 ## dplyr alternative
+library(dplyr)
+
+mean(c(1, 2, NA))
+mean(c(1, 2, NA), na.rm = TRUE)
 
 airquality %>%
   group_by(Month) %>%
@@ -103,26 +123,59 @@ airquality %>%
 ## Merging
 
 shuffle <- sample(nrow(mtcars))
+
 A <- mtcars[shuffle, 5:9]
 B <- mtcars[, 1:4]
 
 merge(x = A, y = B, by = "row.names")
+merge(x = A, y = B, by.x = A$ID, by.y = B$ID2)
+?merge
+
+
+B <- mtcars[1:20, 1:4]
+
+merge(A, B, by = "row.names")
+merge(B, A, by = "row.names")
+
+A$ID1 <- shuffle
+B$ID2 <- seq_len(nrow(B))
+B2 <- B[1:20, ]
+
+merge(A, B2, by.x = "ID1", by.y = "ID2")
 
 ## Note: for dplyr see cheatsheet for joins
 
 ## Binding
 ## Adding rows and columns
+cbind(
+letters[1:10],
+rev(letters[11:20])
+)
+
+rbind(
+letters[1:10],
+rev(letters[11:20])
+)
 
 rbind()
 cbind()
+cbind(A, B2)
 
 ## Binning
 
 library(Hmisc)
 ?cut2
+set.seed(3)
+num <- floor(rnorm(20, 20, 5))
+range(num)
 
-## Renaming factors
-levels(x) <- c("new", "names")
+cut2(num, cuts = 20)
+size <- cut2(num, cuts = c(18, 24))
+
+cut2(num, g = 4)
+
+# Put 10 observations in each group
+cut2(num, m = 10)
 
 ## Transformations 
 
@@ -133,3 +186,4 @@ head(mtcars)
 ## with dplyr
 
 mtcars %>% mutate(lDis = log(disp))
+mtcars %>% transmute(lDis = log(disp))
