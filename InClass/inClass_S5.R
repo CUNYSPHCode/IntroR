cf11df73a23635d2bbe73ad7fa1674c18cefa60c
LiNk-NY <mramos09@gmail.com>
2016-06-16 23:50:40 -0400

    Add notes

--- InClass/inClass_S5.R
+++ InClass/inClass_S5.R
@@ -5,18 +5,22 @@
 
 ## Involved Example
 library(rafalib)
+
 results <- data.frame(OR = c(1.05, 2.04, 9.81, 1.91, 3.54, 0.28),
            lowerCI = c(0.43, 0.74, 3.72, 0.56, 1.24, 0.08),
            higherCI = c(2.55, 5.62, 25.87, 6.55, 10.14, 1.00),
            country = c("Allemange", "France", "Italie", "Royaume-Uni",
                        "Suisse", "Belgique"))
-mypar()
+results
+
+rafalib::mypar()
 
 plot(x = c(1, 10), y = c(1, 6), ylab = "", ylim = c(0, 6),
      type = "n", axes = FALSE, xlab = "Odds Ratio", log = "x",
      xlim = c(0.01, 15))
 
 axis(side = 1, at = c(0.01, 0.5, 1, 2, 5, 10))
+
 points(x= results[, "OR"], y = 1:6, pch = 18)
 segments(x0 = results[["lowerCI"]], x1 = results[["higherCI"]],
          y0 = 1:6, y1 = 1:6)
@@ -50,6 +54,8 @@ broom::tidy(m) %>%
 
 ## ggplot version
 library(ggplot2)
+library(dplyr)
+
 results %>% ggplot(aes(x = reorder(country, OR), y = OR,
                        ymin = lowerCI,
                        ymax = higherCI)) +
@@ -62,6 +68,9 @@ results %>% ggplot(aes(x = reorder(country, OR), y = OR,
 ## Example from Advanced R by Hadley Wickham
 set.seed(1014)
 df <- data.frame(replicate(6, sample(c(1:10, -99), 6, rep = TRUE)))
+
+## Not working but rewritten for reability
+df2 <- c(1:10, -99) %>% sample(6, replace = TRUE) %>% replicate(6, .) %>% data.frame
 names(df) <- letters[1:6]
 
 ## look at df
@@ -78,6 +87,7 @@ df$f[df$g == -99] <- NA
 
 # We can try a loop..
 # Regenerate the data
+set.seed(1014)
 df <- data.frame(replicate(6, sample(c(1:10, -99), 6, rep = TRUE)))
 # See it
 df
@@ -92,11 +102,17 @@ fix_missing <- function(a_vector) {
     return(a_vector)
 }
 
+# Make sure you have -99 in df
+df
+names(df) <- letters[1:6]
 fix_missing(df$d)
 
 ## looking at examples and understanding them
 
-mydatalocation <- "/media/mr148/MR_USB30/data/specdata/"
+## If you're using windows, drives will have letters
+mydatalocation <- "E:/data/specdata/"
+
+list.files(mydatalocation, pattern = ".csv", full.names = TRUE)
 
 complete <- function(directory, id=1:332){
     monitorFile <- list.files(directory, pattern = ".csv", full.names=TRUE)
@@ -104,8 +120,47 @@ complete <- function(directory, id=1:332){
     for (i in id) {
         nobs <- c(nobs, sum(complete.cases(read.csv(monitorFile[i]))))
     }
-    data.frame(id = basename(monitorFile[id]), nobs=nobs)
+    data.frame(filename = basename(monitorFile[id]), nobs=nobs)
 }
 
 complete(mydatalocation, id = 1:3)
+complete(mydatalocation)
 
+## Functionals 
+
+## *apply family
+
+## lapply
+datasets <- list(mtcars, iris, cars)
+
+dim(datasets[[1]])
+dim(datasets[[2]])
+dim(datasets[[3]])
+
+lapply(datasets, dim)
+
+tapply(X = mtcars$mpg, INDEX = mtcars$cyl, FUN = mean)
+tapply(X = mtcars$mpg, INDEX = mtcars$cyl, FUN = sd)
+tapply(X = mtcars$mpg, INDEX = mtcars$cyl, FUN = var)
+
+myfun <- list(meanFUN = function(x) {mean(x, na.rm = TRUE)} ,  sdFUN = sd, varFUN = var)
+names(myfun)
+
+lapply(myfun, function(f) f(mtcars$mpg) )
+
+lapply(mtcars, mean)
+apply(X = mtcars, MARGIN = 2, FUN = mean)
+sapply(mtcars, class)
+
+apply(mtcars, 1, mean)
+apply(mtcars, 1, max)
+
+apply(matrix(rnorm(100), nrow = 10), 2, sd)
+
+apply(matrix(rnorm(100), nrow = 10), 2, sum)
+
+
+vapply(X = list(mtcars, iris, cars),FUN = is.data.frame, FUN.VALUE = logical(1L))
+sapply(X = list(mtcars, iris, cars), FUN = is.data.frame)
+
+vapply(X = list(mtcars, iris, cars),FUN = is.data.frame, FUN.VALUE = character(1L))
